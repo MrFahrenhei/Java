dc5eabe15dcedfdfc0c5260ee4e31263dfac8b8b
Vinícius V. Beraldo <80232541+MrFahrenhei@users.noreply.github.com>
2022-07-09 21:54:12 -0300

    resto do java

---
+++ src/br/fag/edu/lista09/Cliente.java
@@ -0,0 +1,33 @@
+package br.fag.edu.lista09;
+
+public class Clientes {
+
+		private int id;
+		private String descricao;
+		private double price;
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getDescricao() {
+			return descricao;
+		}
+
+		public void setDescricao(String descricao) {
+			this.descricao = descricao;
+		}
+
+		public double getPrice() {
+			return price;
+		}
+
+		public void setPrice(double price) {
+			this.price = price;
+		}
+
+	}
---
+++ src/br/fag/edu/lista09/Estoque.java
@@ -0,0 +1,14 @@
+package br.fag.edu.lista09;
+
+public class Estoque {
+	private int estoque;
+
+	public int getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(int estoque) {
+		this.estoque = estoque;
+	}
+
+}
